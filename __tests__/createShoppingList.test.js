a15f427f63e016d7bffc5b20444faa82c6c8cdb2
Elena Stagg <elenagracestagg@hotmail.co.uk>
2018-09-09 15:18:34 +0100

    completed step 4

--- __tests__/createShoppingList.test.js
+++ __tests__/createShoppingList.test.js
@@ -6,7 +6,7 @@ const createShoppingList = require('../controllers/createShoppingList');
 it('creates a new shopping list', (done) => {
   expect.assertions(1);
   const body = {
-    items: ['broccoli', 'bread', 'bananas'];
+    items: ['broccoli', 'bread', 'bananas'],
   };
   const request = httpMocks.createRequest({
     method: 'POST',
@@ -14,4 +14,18 @@ it('creates a new shopping list', (done) => {
     body: body,
   });
 
-})
+  const response = httpMocks.createResponse({
+    eventEmitter: require('events').EventEmitter,
+  });
+
+  createShoppingList(request, response);
+
+  response.on('end', () => {
+    const filename = response._getData().filename;
+    const filePath = path.join(__dirname, '../controllers/shoppingLists', filename);
+    fs.readFile(filePath, 'utf8', (error, data) => {
+      expect(data).toBe(JSON.stringify(body));
+      done();
+    });
+  });
+});
